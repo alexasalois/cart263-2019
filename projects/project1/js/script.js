83a8fb0227202fbf401d13b030b41fab0a9bcaab
Alexandra Salois <alexasalois@hotmail.com>
2019-02-07 17:33:26 -0500

    P1: Added dialog, goal, and score update
    Pop up window to give instructions, background music, goal score now goes up when you get close to it, infinite!

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -10,23 +10,29 @@ trying without succeeding.
 ******************/
 "use strict"
 
+// set variables for the draggable objects
 let $dragInspiration;
 let $receiveInspiration;
+
+// set variables for the score properties
 let scoreIdeas = 0;
 let scoreTypos = 0;
 let scoreScript = 0;
 let scoreVisuals = 0;
 let scoreInteraction = 0;
+
+// set variables for score to reach
+let goalIdeas = 0;
+let goalTypos = 0;
+let goalScript = 0;
+let goalVisuals = 0;
+let goalInteraction = 0;
+
+// set variable that checks which drag object is being dropped
 let idActive;
 
-// when the page is ready, run this -->
+// when the page is ready, run this
 $(document).ready(setup);
-
-
-// preload the assets
-function preload() {
-
-}
 
 // prepare the code to run
 function setup() {
@@ -38,11 +44,17 @@ function setup() {
 
   // display original stats
   updateStats();
+
+  // pop up dialog for instructions
+  showInstructions();
 }
 
 function retain( event, ui) {
   // Create variable to store the different drag IDs
   idActive = ui.draggable[0].id;
+
+  // play typing sound
+  $('#typingSound').trigger('play');
 
   // Set an interval for the typing animation
   let typeInterval = setInterval(typingComputer, 100);
@@ -75,6 +87,9 @@ function retain( event, ui) {
     scoreIdeas += 1;
     updateStats();
   }
+
+  checkScore();
+
 }
 
 // animate the person at the computer
@@ -88,7 +103,7 @@ function typingComputer() {
 
 
 function updateStats() {
-  // define the variables
+  // define the variables for the number of drag objects, and a parent to contain those objects
   let scores = $(".draggables").length-1;
   let $parent = $("#stats");
 
@@ -103,4 +118,27 @@ function updateStats() {
   $("#stat3").html("Typos avoided: "+scoreTypos);
   $("#stat4").html("Interactions implemented: "+scoreInteraction);
   $("#stat5").html("Lines of script completed: "+scoreScript);
+}
+
+function showInstructions() {
+  $("#first").text("To submit, make sure you have at least "+goalIdeas+" good ideas, avoid "+goalTypos+
+  " typos, write "+goalScript+" lines of script, create "+goalVisuals+" cool visuals, and implement "+goalInteraction
+  +" interactions. Good luck!")
+  $("#first").dialog({
+    width: 450
+  });
+}
+
+function checkScore() {
+  console.log("score?");
+  if ((scoreIdeas > goalIdeas) && (scoreTypos > goalTypos) && (scoreScript > goalScript) && (scoreVisuals > goalVisuals) && (scoreInteraction > goalInteraction)) {
+    goalIdeas += 5;
+    goalTypos += 5;
+    goalScript += 5;
+    goalVisuals += 5;
+    goalInteraction += 5;
+
+    // show pop up with new instructions
+    showInstructions();
+  }
 }
