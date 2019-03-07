81505669f236e8b203ebd2b828d69a773afe499f
Alexandra Salois <alexasalois@hotmail.com>
2019-03-07 13:56:00 -0500

    P2: Folder update

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /*****************
 
 Title of Project
@@ -9,29 +7,3 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-
-// preload()
-//
-// Description of preload
-
-function preload() {
-
-}
-
-
-// setup()
-//
-// Description of setup
-
-function setup() {
-
-}
-
-
-// draw()
-//
-// Description of draw()
-
-function draw() {
-
-}
