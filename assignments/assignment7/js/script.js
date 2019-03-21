982199bb3d03e13020fcf44bb0cb0fdb0fdd9e25
Alexandra Salois <alexasalois@hotmail.com>
2019-03-21 14:58:52 -0400

    A7: Started

---
+++ assignments/assignment7/js/script.js
@@ -0,0 +1,108 @@
+"use strict";
+
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+var frequencies = [
+  220.00,
+  246.94,
+  138.59,
+  146.83,
+  164.81,
+  185.00,
+  207.65
+];
+
+var pattern = [
+  'x o',
+  '*',
+  'x',
+  'x x',
+  'o',
+  'oo',
+  'x',
+  '* * *'
+];
+
+var patternIndex = 0;
+
+var synthFX;
+var kickFX;
+var snareFX;
+var hihatFX;
+
+function preload() {
+//
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+    synthFX= new Pizzicato.Sound({
+    source: 'wave',
+  });
+
+   hihatFX = new Pizzicato.Sound('assets/sounds/hihat.wav');
+
+   kickFX = new Pizzicato.Sound('assets/sounds/kick.wav');
+
+   snareFX = new Pizzicato.Sound('assets/sounds/snare.wav');
+
+  playNote();
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
+
+function playNote() {
+  var newFrequency = random(frequencies);
+  synthFX.frequency = newFrequency;
+  synthFX.play();
+}
+
+function mousePressed() {
+  setInterval(playNote,500);
+  setInterval(playDrum,250);
+}
+
+function playDrum() {
+  var symbols = pattern[patternIndex];
+
+  if (symbols.indexOf('x') !== -1) {
+    kickFX.play();
+  }
+
+  if (symbols.indexOf('o') !== -1) {
+    snareFX.play();
+  }
+
+  if (symbols.indexOf('*') !== -1) {
+    hihatFX.play();
+  }
+
+  patternIndex++;
+
+  if (patternIndex === pattern.length) {
+    patternIndex = 0;
+  }
+}
